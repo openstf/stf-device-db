9d3a86d61d02e3382fe8448d380890448a833afd
Valverde Antonio <valverde_antonio@cyberagent.co.jp>
2014-06-16 19:13:27 +0900

    Some corrections

--- mapping.js
+++ mapping.js
@@ -1,7 +1,6 @@
 module.exports = function(properties) {
   switch (properties.model) {
     case 'Nexus 5':
-      return properties.model + '|' + properties.name
     case 'Nexus 7':
       return properties.model + '|' + properties.name
     default:
