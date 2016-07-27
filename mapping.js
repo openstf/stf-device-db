ce614532922154cea51f0696f43aba69c99ad9e6
joaopedroms22 <joaopedrosilva96@hotmail.com>
2016-07-27 16:12:03 +0100

    Update mapping.js
    Changed indentation of file.

--- mapping.js
+++ mapping.js
@@ -1,8 +1,8 @@
 module.exports = function(properties) {
   if (properties.model) {
-	//This line of code, removes all spaces in the beggining of each properties.model,
-	//Example ' One M8' -> 'One_M8' instead ' One M8' -> '_One_M8'   
-	properties.model = properties.model.replace(/^\s+/, '');
+    //This line of code, removes all spaces in the beggining of each properties.model,
+    //Example ' One M8' -> 'One_M8' instead ' One M8' -> '_One_M8'   
+    properties.model = properties.model.replace(/^\s+/, '');
     switch (properties.model) {
       case 'Nexus 7':
         switch (properties.name) {
@@ -10,7 +10,7 @@ module.exports = function(properties) {
           case 'nakasi':
             return 'Nexus_7_2012'
           case 'razor':
-	        return 'Nexus_7'
+	    return 'Nexus_7'
           case 'razorg':
             return 'Nexus_7_2013'
         }
